ef2bf9665edc17de6aba11bbe0f7b1d8e63662b3
Sharen Mathew <sharenmathew89@gmail.com>
2024-02-12 23:04:31 +0530

    model created

--- src/DimondPricePrediction/components/data_ingestion.py
+++ src/DimondPricePrediction/components/data_ingestion.py
@@ -16,33 +16,43 @@ class DataIngestionConfig:
     test_data_path:str=os.path.join("artifacts","test.csv")
 
 
-
 class DataIngestion:
     def __init__(self):
         self.ingestion_config=DataIngestionConfig()
         
+    
     def initiate_data_ingestion(self):
         logging.info("data ingestion started")
         
         try:
             data=pd.read_csv(Path(os.path.join("notebooks/data","train.csv")))
-            logging.info("read data as df")
+            logging.info(" i have read dataset as a df")
+            
             
             os.makedirs(os.path.dirname(os.path.join(self.ingestion_config.raw_data_path)),exist_ok=True)
             data.to_csv(self.ingestion_config.raw_data_path,index=False)
-            logging.info("saved raw data path")
+            logging.info(" i have saved the raw dataset in artifact folder")
             
             logging.info("here i have performed train test split")
+            
             train_data,test_data=train_test_split(data,test_size=0.25)
             logging.info("train test split completed")
-            
             
             train_data.to_csv(self.ingestion_config.train_data_path,index=False)
             test_data.to_csv(self.ingestion_config.test_data_path,index=False)
             
+            logging.info("data ingestion part completed")
+            
+            return (
+                 
+                
+                self.ingestion_config.train_data_path,
+                self.ingestion_config.test_data_path
+            )
+            
             
         except Exception as e:
-            logging.info("exception occured during ingestion of data")
-            raise customexception(e,sys)
+           logging.info("exception during occured at data ingestion stage")
+           raise customexception(e,sys)
             
         
