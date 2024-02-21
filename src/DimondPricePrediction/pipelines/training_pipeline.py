42e214eaa350d2844e3e6929619b0ab70932dd4a
Sharen Mathew <sharenmathew89@gmail.com>
2024-02-21 16:18:33 +0530

    updated changes

--- src/DimondPricePrediction/pipelines/training_pipeline.py
+++ src/DimondPricePrediction/pipelines/training_pipeline.py
@@ -1,6 +1,7 @@
 from src.DimondPricePrediction.components.data_ingestion import DataIngestion
 from src.DimondPricePrediction.components.data_transformation import DataTransformation
 from src.DimondPricePrediction.components.model_trainer import ModelTrainer
+from src.DimondPricePrediction.components.model_evaluation import ModelEvaluation
 
 import os
 import sys
@@ -17,3 +18,6 @@ train_arr,test_arr=data_transformation.initialize_data_transformation(train_data
 
 data_model_obj=ModelTrainer()
 data_model_obj.initate_model_training(train_arr,test_arr)
+
+model_eval_obj=ModelEvaluation()
+model_eval_obj.initiate_model_evaluation(train_arr,test_arr)
